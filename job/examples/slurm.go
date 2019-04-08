d152a25c7a55e4ee920fb35978640d7c005b72bd
Jason.Yu <morty.yu@yahoo.com>
2019-04-08 17:22:43 +0800

    config use viper

--- job/examples/slurm.go
+++ job/examples/slurm.go
@@ -3,17 +3,48 @@ package main
 import (
 	"log"
 	"os"
+	// "fmt"
+	"bytes"
 
 	"github.com/scut-ccmp/flowmat/job"
+	"github.com/spf13/viper"
 )
 
 
 func main() {
 
-	host := "202.38.220.15"
-	port := "22"
-	user := "unkcpz"
-	pass := "sunshine"
+	var tomlExample = []byte(`
+{
+	"server": {
+		"host": "202.38.220.15",
+		"port": "22",
+		"user": "unkcpz",
+		"password": "sunshine"
+	},
+	"file": {
+		"tempDir": "/home/unkcpz/giida",
+		"dirPrefix": "tmp"
+	}
+}
+	`)
+
+	viper.SetConfigName("config")
+	viper.AddConfigPath("$HOME/.config/job/")
+	viper.AddConfigPath(".")
+	viper.SetConfigType("json")
+	// err := viper.ReadInConfig()
+	viper.ReadConfig(bytes.NewBuffer(tomlExample))
+	// if err != nil { // Handle errors reading the config file
+	// 	panic(fmt.Errorf("Fatal error config file: %s \n", err))
+	// }
+
+	user := viper.GetString("server.user")
+	pass := viper.GetString("server.password")
+	host := viper.GetString("server.host")
+	port := viper.GetString("server.port")
+
+	dir := viper.GetString("server.tempDir")
+	prefix := viper.GetString("server.dirPrefix")
 
 	// get host public key
 	// hostKey := getHostKey(host)
@@ -24,8 +55,6 @@ func main() {
 	}
 	defer conn.Close()
 
-	dir := "/home/unkcpz/giida"
-	prefix := "tmp"
 	pathname, err := job.TempDir(conn.Client, dir, prefix)
 	if err != nil {
 		log.Fatal(err)
